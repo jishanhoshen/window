8541ca8b8851df7deaa78da1f3225c33f31d860a
Jishan Hoshen <70538593+jishanhoshen@users.noreply.github.com>
2020-08-04 16:43:58 +0600

    window

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body id="fullscreen">
+<?php
+include 'fullscreen.html';
+include 'windows_start.html';
+echo '
+</body>
+</html>
+';
